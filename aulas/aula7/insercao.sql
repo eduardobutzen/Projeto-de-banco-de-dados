9c0ad0877c495b04e700b0a3084d2f5e7273d33d
eduardobutzen <eduardo.butzen@ufn.edu.br>
2026-05-04 09:21:03 -0300

    aula7

---
+++ aulas/aula7/insercao.sql
@@ -0,0 +1,15 @@
+show databases;
+use mydb;
+
+-- Selecionar tabela:
+select *
+from atleta;
+
+-- inserrir nome na tabela (id, nome):
+insert into atleta values (1, "Eduardo");
+
+-- inserir nome apenas pela coluna nome (id com auto incremento):
+insert into atleta (nome) VALUES ("Pedro");
+
+-- Deletar linha onde idAtleta for igual a 3:
+delete from atleta where idAtleta = 3;
